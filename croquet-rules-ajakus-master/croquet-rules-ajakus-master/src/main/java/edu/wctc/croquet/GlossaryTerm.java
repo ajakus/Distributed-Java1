52b43bb4f0bee085f96e13a0463801a717d31a15
ajakus <60129215+ajakus@users.noreply.github.com>
2022-02-21 03:46:13 -0600

    Add files via upload

---
+++ croquet-rules-ajakus-master/croquet-rules-ajakus-master/src/main/java/edu/wctc/croquet/GlossaryTerm.java
@@ -0,0 +1,20 @@
+package edu.wctc.croquet;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
+/**
+ * Complete this class to hold data from croquetGlossary.json
+ * Do I need to do something else to store the data in these variables? other methods?
+ */
+public class GlossaryTerm {
+    private String term;
+    private String definition;
+    private String regulation;
+
+
+
+}
